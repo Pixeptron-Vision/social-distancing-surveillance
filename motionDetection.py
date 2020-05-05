d5dfd4ae404a5a566a84358fc5ed3dbb6c1b8125
mukuakhilesh <akhi.biada007@gmail.com>
2020-05-05 12:58:44 +0530

    Add seperate thread for frame grabbing

--- motionDetection.py
+++ motionDetection.py
@@ -2,25 +2,31 @@ import numpy as np
 import argparse
 import cv2
 import imutils
+import sys
+from videocaptureasync import VideoCaptureAsync  #VideoCaptureAsync implements separate thread for reading stream from camera
 
+# The below function is responsible for implementing the Detecting the motion.
 def detectMotion(cap):
-    
+
     print("Reading first frame...")
-    ret, image = cap.read()
+    ret, image = cap.read()             
     image = cv2.resize(image,(640,480))
     cv2.waitKey(1)
     print("First frame Read")
-    if  not ret:
+    if  not ret or image is None:
         print("[INFO] Stream unavailable..!")
-        cap.release()
-        return False
-    if ret:
-        frameIds = cap.get(cv2.CAP_PROP_FRAME_COUNT) * np.random.uniform(size=25)
+        #cap.release()
+        cap.stop()
+        return False        #Return Flase to shows that the stream has breaked in between ot video file has reached end.
+    #if ret:
+    else:
+        # cap.cap is same as the object/instance of VideoCapture() used before. See videocaptureasync.py file for more understanding
+        frameIds = cap.cap.get(cv2.CAP_PROP_FRAME_COUNT) * np.random.uniform(size=25) 
         # Display median frame
         # Store selected frames in an array
         frames = []
         for fid in frameIds:
-            cap.set(cv2.CAP_PROP_POS_FRAMES, fid)
+            cap.cap.set(cv2.CAP_PROP_POS_FRAMES, fid)
             ret, frame = cap.read()
             frame = cv2.resize(frame,(640,480))
             frames.append(frame)
@@ -29,15 +35,19 @@ def detectMotion(cap):
             grayMedianFrame = cv2.cvtColor(medianFrame, cv2.COLOR_BGR2GRAY)
             grayMedianFrame = cv2.GaussianBlur(grayMedianFrame, (21, 21), 0)
     #out = cv2.VideoWriter('output.avi', -1, 20.0, (640,480))
-    while(True):
+    
+    #Run the while loop till the camera is connected/port remain opened
+    while (cap.isOpened()):
         #print "In loop"
-        ret, frame = cap.read()
-        if not ret:
+        ret, frame = cap.read() 
+        if  not ret or image is None:
             print("[INFO] Cam IP Stream unavailable...")
-            cap.release()
+            #cap.release()
+            #cap.stop() is responsible for stopping the capturing thread part and releasing the camera free
+            cap.stop()
             return False
-
-        if ret:
+        #if ret:
+        else :
 
             frame = cv2.resize(frame,(640,480))
             cframe = np.copy(frame)
@@ -91,36 +101,45 @@ def detectMotion(cap):
             #out.write(image)
             """
         if cv2.waitKey(1) & 0xFF == ord('q'):
+            cap.stop()
+            cv2.destroyAllWindows()
+            sys.exit(0)  
             break
     #cv2.waitKey(0)
-    cap.release()
+    #cap.release()
     #out.release()
+    cap.stop()
     cv2.destroyAllWindows()
+    return True         #Returns True to display succesfull end of function and prevents returning garbage value
 
 # Function for error display if stream is unavailable at start or camera port is inaccessible.
 def display_stream_error():
     # cv2.destroyWindow('Camera Stream')
-    cv2.destroyWindow('Motion')
+    cv2.destroyWindow('Motion')      # Destroys the rdundant gui window               
     error_img = cv2.imread(r'stream_error.jpg')
     error_img = cv2.resize(error_img, (500,500), interpolation=cv2.INTER_AREA)
     print('Camera disconnected')
-    cap.release()
-    cv2.imshow('Stream',error_img)
+    #cap.release()
+    cv2.imshow('Stream',error_img) 
     cv2.waitKey(3000)                 # A 3 second refresh/halt to recheck connections
 
 
 while True:
-    #cap = cv2.VideoCapture('http://service:Tata!123@192.168.51.77/video.mp4?line=1&inst=1&rec=0&rnd=60779') #service:TATA!@123;service:Pass!234; 48.51  ;rtsp:/service:TATA!@123@192.168.48.51
+    #cap = cv2.VideoCapture('http://service:Tata!123@192.168.51.77/video.mp4?line=1&inst=1&rec=0&rnd=60779') #service:TATA!@123;service:Pass!234; 48.51           ;rtsp:/service:TATA!@123@192.168.48.51
     # rtsp://192.168.48.51//rtsp_tunnel , rtsp:/service:TATA!@123@192.168.48.51
     # print("Reading first frame...") #http://192.168.51.77/video.mp4?line=1&inst=1&rec=0&rnd=37660
     # ret, image = cap.read() #http://192.168.51.77/video.mp4?line=1&inst=1&rec=0&rnd=60779
     
-    cap = cv2.VideoCapture(0)
+    
+    #Note: VideoCaptureAsync implemented here has same format as VideoCapture....just specify the link of ip cam as:
+    # cap = VideoCaptureAsync(src="videofile_name / Ip camera link")
+    cap = VideoCaptureAsync(src=0)            
+    cap.start()             # This is responsible for starting up the thread and frame capturing process
     if cap.isOpened():
         print('Camera is connected')
         response = detectMotion(cap)
         if response == False:
-            display_stream_error()
+            display_stream_error() #Call the function to display the error message
             continue
     else:                                # If system is unable to open/access the camera port.
         display_stream_error()
