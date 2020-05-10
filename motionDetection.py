702ae47a9cf6c3c892fdcd8496b7295b58c501e0
Shouvik Patra <shouvikpatra@gmail.com>
2020-05-10 15:01:02 +0530

    Modified background updation

--- motionDetection.py
+++ motionDetection.py
@@ -4,7 +4,8 @@ import cv2
 import imutils
 import sys
 from scipy.spatial import distance
-from visionObjects.videocaptureasync import VideoCaptureAsync  #VideoCaptureAsync implements separate thread for reading stream from camera
+# VideoCaptureAsync implements separate thread for reading stream from camera
+from visionObjects.videocaptureasync import VideoCaptureAsync
 from visionObjects.distanceCalc import d_Calc
 # The below function is responsible for implementing the Detecting the motion.
 desired_time_frame = 1
@@ -13,28 +14,50 @@ threshold_dist = 75
 # For background frame, intialising a background_frames dictionary
 background_frames = {}
 motion_status = False
+mean_background_frame = None
+
+
+def updateBackground(background_frames, frame_rate, desired_time_frame):
+    # if no of frames in background_frames > threshold no of frames to update
+    global mean_background_frame
+    if len(background_frames) > frame_rate * desired_time_frame:
+        # Taking the mean of background frames using the frame id
+        background_id = []
+        for i in sorted(background_frames.keys()):
+            background_id.append(i)
+        np_background_id = np.array(background_id)
+        # mean frame id of background
+        mean_id = int(np.mean(np_background_id))
+        np_background_id = np_background_id[np_background_id >= mean_id]
+        # mean background frame itself
+        mean_background_frame = background_frames[np_background_id[0]]
+        # making the dictionary empty
+        background_frames.clear()
+
 
 def detectMotion(cap):
 
     print("Reading first frame...")
     ret, frame1 = cap.read()
-    fid=1
+    fid = 1
     frame1 = cv2.resize(frame1, (640, 480))
+    global mean_background_frame
     mean_background_frame = frame1
     print("First frame Read")
-    if  not ret or frame1 is None:
+    if not ret or frame1 is None:
         print("[INFO] Stream unavailable..!")
         cap.stop()
-        return False        #Return Flase to shows that the stream has breaked in between ot video file has reached end.
+        # Return Flase to shows that the stream has breaked in between ot video file has reached end.
+        return False
 
     while cap.isOpened():
         # second and subsequent frames
         ret, frame2 = cap.read()
-        if  not ret or frame2 is None:
+        if not ret or frame2 is None:
             print("[INFO] Cam IP Stream unavailable...")
             cap.stop()
             return False
-        else :
+        else:
             motion_status = False
             # Resizing the frame
             frame1 = cv2.resize(frame1, (640, 480))
@@ -48,28 +71,16 @@ def detectMotion(cap):
             dilated = cv2.dilate(threst_delta, None, iterations=3)
             contours, _ = cv2.findContours(dilated,
                                            cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-            print(f"Number of contours found in {fid} frame = { len(contours) }")
-           
-            d_obj = d_Calc(frame1 , threshold_dist , motion_status , contours)
-            motion_status = d_obj.CalcDist(frame1 , threshold_dist , motion_status , contours)
-            
+            #print(f"Number of contours found in {fid} frame = { len(contours) }")
+
+            d_obj = d_Calc(frame1, threshold_dist, motion_status, contours)
+            motion_status = d_obj.CalcDist(
+                frame1, threshold_dist, motion_status, contours)
+
 #            Changing background dynamically
             if motion_status:
-        # if no of frames in background_frames > threshold no of frames to update
-                if len(background_frames) > frame_rate * desired_time_frame:
-            # Taking the mean of background frames using the frame id
-                    background_id = []
-                    for i in sorted(background_frames.keys()):
-                        background_id.append(i)
-                    np_background_id = np.array(background_id)
-            # mean frame id of background
-                    mean_id = int(np.mean(np_background_id))
-                    np_background_id = np_background_id[np_background_id >= mean_id]
-            # mean background frame itself
-                    mean_background_frame = background_frames[np_background_id[0]]
-            # making the dictionary empty
-                    background_frames.clear()
-
+                updateBackground(
+                    background_frames, frame_rate, desired_time_frame)
     # No motion detected in frame1, so add to dictionary of background frame
             if not motion_status:
                 background_frames[fid] = frame1
@@ -84,20 +95,17 @@ def detectMotion(cap):
             fid += 1
             #ret, frame2 = video.read()
 
-
         if cv2.waitKey(1) & 0xFF == ord('q'):
             cap.stop()
             cv2.destroyAllWindows()
             sys.exit(0)
             break
-    #cv2.waitKey(0)
-    #cap.release()
-    #out.release()
+    # cv2.waitKey(0)
+    # cap.release()
+    # out.release()
     cap.stop()
     cv2.destroyAllWindows()
-    return True         #Returns True to display succesfull end of function and prevents returning garbage value
-
-
+    return True  # Returns True to display succesfull end of function and prevents returning garbage value
 
 
 # Function for error display if stream is unavailable at start or camera port is inaccessible.
@@ -105,34 +113,33 @@ def display_stream_error():
     # cv2.destroyWindow('Camera Stream')
     cv2.destroyWindow('Motion')      # Destroys the rdundant gui window
     error_img = cv2.imread(r'stream_error.jpg')
-    error_img = cv2.resize(error_img, (500,500), interpolation=cv2.INTER_AREA)
+    error_img = cv2.resize(error_img, (500, 500), interpolation=cv2.INTER_AREA)
     print('Camera disconnected')
-    #cap.release()
-    cv2.imshow('Stream',error_img)
-    cv2.waitKey(1000)                 # A 3 second refresh/halt to recheck connections
-
-
+    # cap.release()
+    cv2.imshow('Stream', error_img)
+    # A 3 second refresh/halt to recheck connections
+    cv2.waitKey(1000)
 
 
 while True:
-    #cap = cv2.VideoCapture('http://service:Tata!123@192.168.51.77/video.mp4?line=1&inst=1&rec=0&rnd=60779') #service:TATA!@123;service:Pass!234; 48.51           ;rtsp:/service:TATA!@123@192.168.48.51
+    # cap = cv2.VideoCapture('http://service:Tata!123@192.168.51.77/video.mp4?line=1&inst=1&rec=0&rnd=60779') #service:TATA!@123;service:Pass!234; 48.51           ;rtsp:/service:TATA!@123@192.168.48.51
     # rtsp://192.168.48.51//rtsp_tunnel , rtsp:/service:TATA!@123@192.168.48.51
     # print("Reading first frame...") #http://192.168.51.77/video.mp4?line=1&inst=1&rec=0&rnd=37660
     # ret, image = cap.read() #http://192.168.51.77/video.mp4?line=1&inst=1&rec=0&rnd=60779
-
-
-    #Note: VideoCaptureAsync implemented here has same format as VideoCapture....just specify the link of ip cam as:
+    # Note: VideoCaptureAsync implemented here has same format as VideoCapture....just specify the link of ip cam as:
     # cap = VideoCaptureAsync(src="videofile_name / Ip camera link")
     # cap = VideoCaptureAsync(src='PNNLParkingLot2.avi')
-    cap = VideoCaptureAsync(src="PNNL_Parking_LOT(1).avi")
-    cap.start()             # This is responsible for starting up the thread and frame capturing process
+    cap = VideoCaptureAsync(src="cctv(2).mp4")
+    # This is responsible for starting up the thread and frame capturing process
+    cap.start()
     if cap.isOpened():
         print('Camera is connected')
         response = detectMotion(cap)
         if response == False:
-            display_stream_error() #Call the function to display the error message
+            display_stream_error()  # Call the function to display the error message
             continue
-    else:                                # If system is unable to open/access the camera port.
+    # If system is unable to open/access the camera port.
+    else:
         display_stream_error()
         continue
 
--- visionObjects/distanceCalc.py
+++ visionObjects/distanceCalc.py
@@ -4,41 +4,41 @@ import numpy as np
 
 
 class d_Calc:
-    def __init__(self , frame1 , threshold_dist , motion_status , contours):
+    def __init__(self, frame1, threshold_dist, motion_status, contours):
         self.frame1 = frame1
-        self.threshold_dist = threshold_dist 
+        self.threshold_dist = threshold_dist
         self.motion_status = motion_status
         self.contours = contours
-    
-    def CalcDist(self , frame1 , threshold_dist , motion_status , contours):
+
+    def CalcDist(self, frame1, threshold_dist, motion_status, contours):
         center = []
         for cnt in contours:
-        # filtering out which contours to ignore
+            # filtering out which contours to ignore
             if cv2.contourArea(cnt) < 500:
                 continue
             motion_status = True
 #            cframe1 = frame1.copy()
         # Drawing a rectangular box around the bounding points of contour
             (x, y, w, h) = cv2.boundingRect(cnt)
-            cv2.rectangle(frame1, (x-1, y-1), (x+w-1-1, y+h-1-1), (255, 0, 0), 3)
-        
+            cv2.rectangle(frame1, (x-1, y-1),
+                          (x+w-1-1, y+h-1-1), (255, 0, 0), 3)
+
             M = cv2.moments(cnt)
             cx = int(M['m10'] / M['m00'])
             cy = int(M['m01'] / M['m00'])
             center.append([cx, cy])
-        
+
 #        cv2.putText(frame1, "Status: {}".format("Motion Detected"),
 #                    (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
     #cv2.drawContours(frame1, countours, -1, (255, 0, 0), 3)
-        print(center)
+        # print(center)
         N = len(center)
-        if N>1:
-        
-        # Find distance between all pairs
+        if N > 1:
+
+            # Find distance between all pairs
             D = distance.pdist(center)
             D = distance.squareform(D)
         # D
-        
 
         # ignore the upper triangle as the matrix will be symmetric so yo avoid repetation of pairs
             upper_tri = np.triu_indices(N, 0)
@@ -56,5 +56,4 @@ class d_Calc:
                                     center[person_2_id[i]][1]), 10, (0, 0, 255), -1)
             # Displaying the frames
             cv2.imshow("Frame", frame1)
-            return motion_status
-        
+        return motion_status
