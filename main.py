afee8c907dce7b09611a221958f6c49ed257c287
mukuakhilesh <akhi.biada007@gmail.com>
2020-05-17 13:21:58 +0530

    migrated motionDetection to main.py

--- main.py
+++ main.py
@@ -1,29 +1,116 @@
+import numpy as np
+import argparse
 import cv2
+import imutils
+import sys
+from scipy.spatial import distance
+import datetime as dt
+import time
+from threading import Thread
 # VideoCaptureAsync implements separate thread for reading stream from camera
 from visionObjects.videocaptureasync import VideoCaptureAsync
+from visionObjects.frameDisplay import FrameDisplay
+from visionObjects.distanceCalc import calculate_dist
+from visionObjects.backgroundDetection import background
 from visionObjects.streamError import display_stream_error
-from visionObjects.detectMotion import detectMotionClass
+from visionObjects.activityDetection import activity_filter , calc_centers
+# The below function is responsible for implementing the Detecting the motion.
+timeDuration = dt.timedelta(seconds=40)
+desired_time_frame = int(timeDuration.seconds)
+#desired_time_frame = 10
+frame_rate = 15
+threshold_dist = 75
+# For background frame, intialising a background_frames dictionary
+background_frames = {}
+motion_status = False
+mean_background_frame = None
 
-while True:
-    # cap = cv2.VideoCapture('http://service:Tata!123@192.168.51.77/video.mp4?line=1&inst=1&rec=0&rnd=60779') #service:TATA!@123;service:Pass!234; 48.51           ;rtsp:/service:TATA!@123@192.168.48.51
-    # rtsp://192.168.48.51//rtsp_tunnel , rtsp:/service:TATA!@123@192.168.48.51
-    # print("Reading first frame...") #http://192.168.51.77/video.mp4?line=1&inst=1&rec=0&rnd=37660
-    # ret, image = cap.read() #http://192.168.51.77/video.mp4?line=1&inst=1&rec=0&rnd=60779
-    # Note: VideoCaptureAsync implemented here has same format as VideoCapture....just specify the link of ip cam as:
-    # cap = VideoCaptureAsync(src="videofile_name / Ip camera link")
-    # cap = VideoCaptureAsync(src='cctv(2).mp4')
-    cap = VideoCaptureAsync(src=0)
-    # This is responsible for starting up the thread and frame capturing process
-    cap.start()
-    if cap.isOpened():
-        print('Camera is connected')
-        d_motion = detectMotionClass(cap)
-        response = d_motion.detectMotion(cap)
-        if response == False:
-            display_stream_error()  # Call the function to display the error message
-            continue
-    # If system is unable to open/access the camera port.
+
+def detectMotion(cap):
+
+    ret, frame1 = cap.read()
+    if not ret or frame1 is None:
+        print("[INFO] Stream unavailable..!")
+        user_exit = display_obj.display_error()
+        if user_exit==True:
+            cap.stop()
+            display_obj.stop()
+            sys.exit(0)
+
     else:
-        display_stream_error()
+        fid = 1
+        frame1 = cv2.resize(frame1, (640, 480))
+        # Set first frame as default background
+        background_frame = frame1
+        # Initialize background update object and pass first frame
+        backUpdate_obj = background(frame1)
+        # Set previous frame to first frmae for starting conditions
+        prev_frame=frame1
 
-cv2.destroyAllWindows()
+        print("First frame Read")
+
+        while True:
+            # Read second and subsequent frames
+            beg = time.time()
+            ret, current_frame = cap.read()
+            if not ret or current_frame is None:
+                print("[INFO] Cam IP Stream unavailable...")
+                user_exit = display_obj.display_error()
+
+            else:
+                print("fid - ",fid)
+                motion_status = False
+                # Resizing the frame
+                prev_frame = cv2.resize(prev_frame, (640, 480))
+                current_frame = cv2.resize(current_frame, (640, 480))
+                # back_append_thread = backUpdate_obj.back_append_thread(current_frame)
+
+                # Update the background and append current frame if no motion
+                backUpdate_obj.background_filter(current_frame)
+                backUpdate_obj.update_background()
+
+                background_frame = backUpdate_obj.get_background_frame()
+
+                motion_frame = backUpdate_obj.get_motion_frame()
+
+                # Detect activity from current_frame and background_frame
+                contours,(centres,boxed_current_frame) = activity_filter(current_frame,background_frame)
+                # Returns id of pairs violating the norms
+                pairs = calculate_dist(boxed_current_frame,centres,threshold_dist)
+                # Note: pairs has id index of contour center in centres list
+                # persion_1_id = pairs[0] - a nx1 array
+                # persion_2_id = pairs[1] - a nx1 array
+
+                # Disaply the frames and read is user presses q/exit
+                user_exit = display_obj.update(boxed_current_frame,motion_frame,background_frame)
+
+                # Choosing next frame
+                prev_frame = current_frame
+                fid += 1
+            if user_exit==True:
+                cap.stop()
+                display_obj.stop()
+                sys.exit(0)
+
+        cap.stop()
+        display_obj.stop()
+        sys.exit(0)
+
+# Intializing and starting the UI thread object
+display_obj = FrameDisplay().start()
+
+# cap = cv2.VideoCapture('http://service:Tata!123@192.168.51.77/video.mp4?line=1&inst=1&rec=0&rnd=60779') #service:TATA!@123;service:Pass!234; 48.51           ;rtsp:/service:TATA!@123@192.168.48.51
+# rtsp://192.168.48.51//rtsp_tunnel , rtsp:/service:TATA!@123@192.168.48.51
+# print("Reading first frame...") #http://192.168.51.77/video.mp4?line=1&inst=1&rec=0&rnd=37660
+# ret, image = cap.read() #http://192.168.51.77/video.mp4?line=1&inst=1&rec=0&rnd=60779
+
+# Note: VideoCaptureAsync implemented here has same format as VideoCapture....just specify the link of ip cam as:
+# cap = VideoCaptureAsync(src="videofile_name / Ip camera link")
+cap = VideoCaptureAsync(src='../PNNLParkingLot2.avi')
+# cap = VideoCaptureAsync(src=0)
+# This is responsible for starting up the thread and frame capturing process
+cap.start()
+while True:
+    detectMotion(cap)
+
+# cv2.destroyAllWindows()
--- motionDetection.py
+++ motionDetection.py
@@ -5,13 +5,15 @@ import imutils
 import sys
 from scipy.spatial import distance
 import datetime as dt
+import time
+from threading import Thread
 # VideoCaptureAsync implements separate thread for reading stream from camera
 from visionObjects.videocaptureasync import VideoCaptureAsync
 from visionObjects.frameDisplay import FrameDisplay
-from visionObjects.distanceCalc import d_Calc
-from visionObjects.backgroundDetection import backgroundCalc
+from visionObjects.distanceCalc import calculate_dist
+from visionObjects.backgroundDetection2 import background
 from visionObjects.streamError import display_stream_error
-
+from visionObjects.activityDetection import activity_filter , calc_centers
 # The below function is responsible for implementing the Detecting the motion.
 timeDuration = dt.timedelta(seconds=40)
 desired_time_frame = int(timeDuration.seconds)
@@ -39,63 +41,51 @@ def detectMotion(cap):
         fid = 1
         frame1 = cv2.resize(frame1, (640, 480))
         # Set first frame as default background
-        mean_background_frame = frame1
+        background_frame = frame1
+        # backUpdate_obj.mean_background_frame = frame1
+        # backUpdate_obj.previous_frame = frame1
+        # backUpdate_obj.append_static_frame(frame1, backUpdate_obj.fid)
+        # backUpdate_obj = background(frame1)
+        # backUpdate_obj.start()
+
+        prev_frame=frame1
 
         print("First frame Read")
 
         while True:
             # Read second and subsequent frames
-            ret, frame2 = cap.read()
-            if not ret or frame2 is None:
+            beg = time.time()
+            ret, current_frame = cap.read()
+            if not ret or current_frame is None:
                 print("[INFO] Cam IP Stream unavailable...")
                 user_exit = display_obj.display_error()
 
             else:
-                # print("fid - ",fid)
+                print("fid - ",fid)
                 motion_status = False
                 # Resizing the frame
-                frame1 = cv2.resize(frame1, (640, 480))
-                frame2 = cv2.resize(frame2, (640, 480))
-                # Applying appropiate filters
-                frame = cv2.absdiff(frame1, frame2)
-                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-                blur = cv2.GaussianBlur(gray, (5, 5), 0)
-                threst_delta = cv2.threshold(blur, 10, 255, cv2.THRESH_BINARY)[1]
-                dilated = cv2.dilate(threst_delta, None, iterations=1)
-                contours, _ = cv2.findContours(dilated,
-                                               cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-                #print(f"Number of contours found in {fid} frame = { len(contours) }")
-
-                d_obj = d_Calc(frame1, threshold_dist, motion_status, contours)
-                motion_status = d_obj.CalcDist(
-                    frame1, threshold_dist, motion_status, contours)
-
-
-                b_obj = backgroundCalc(
-                    background_frames, mean_background_frame, frame_rate, desired_time_frame)
-                # Changing background dynamically
-                if motion_status:
-                    mean_background_frame = b_obj.updateBackground(
-                        background_frames, mean_background_frame, frame_rate, desired_time_frame)
-
-                # No motion detected in frame1, so add to dictionary of background frame
-                if not motion_status:
-                    background_frames[fid] = frame1
-                    # If no motion is detected for a long time
-                    if len(background_frames) > 2 * frame_rate * desired_time_frame:
-                        mean_background_frame = b_obj.updateBackground(
-                            background_frames, mean_background_frame, frame_rate, desired_time_frame)
-
-                # Displaying the frames
-                user_exit = display_obj.update(frame1,threst_delta,mean_background_frame)
+                prev_frame = cv2.resize(prev_frame, (640, 480))
+                current_frame = cv2.resize(current_frame, (640, 480))
+                print(time.time()-beg)
+                # back_append_thread = backUpdate_obj.back_append_thread(current_frame)
+                # backUpdate_obj.background_filter(current_frame)
+                #
+                # background_frame = backUpdate_obj.get_background_frame()
+                # motion_frame = backUpdate_obj.get_motion_frame()
+                contours,centres = activity_filter(current_frame,background_frame)
+                print(time.time()-beg)
+                pairs = calculate_dist(current_frame,centres,threshold_dist)
+                print(time.time()-beg)
+                user_exit = display_obj.update(current_frame,None,background_frame)
                 # Choosing next frame
-                frame1 = frame2
+                print(time.time()-beg)
+                prev_frame = current_frame
                 fid += 1
-
             if user_exit==True:
                 cap.stop()
                 display_obj.stop()
+                backUpdate_obj.stop()
+                # back_append_thread.join()
                 sys.exit(0)
                 # break
 
@@ -104,8 +94,9 @@ def detectMotion(cap):
         sys.exit(0)
 
 # Intializing and starting the UI thread object
-display_obj = FrameDisplay()
-display_obj.start()
+display_obj = FrameDisplay().start()
+backUpdate_obj = background()
+# backUpdate_obj.start()
 # cap = cv2.VideoCapture('http://service:Tata!123@192.168.51.77/video.mp4?line=1&inst=1&rec=0&rnd=60779') #service:TATA!@123;service:Pass!234; 48.51           ;rtsp:/service:TATA!@123@192.168.48.51
 # rtsp://192.168.48.51//rtsp_tunnel , rtsp:/service:TATA!@123@192.168.48.51
 # print("Reading first frame...") #http://192.168.51.77/video.mp4?line=1&inst=1&rec=0&rnd=37660
@@ -113,8 +104,8 @@ display_obj.start()
 
 # Note: VideoCaptureAsync implemented here has same format as VideoCapture....just specify the link of ip cam as:
 # cap = VideoCaptureAsync(src="videofile_name / Ip camera link")
-# cap = VideoCaptureAsync(src='../PNNLParkingLot2.avi')
-cap = VideoCaptureAsync(src=0)
+cap = VideoCaptureAsync(src='../PNNLParkingLot2.avi')
+# cap = VideoCaptureAsync(src=0)
 # This is responsible for starting up the thread and frame capturing process
 cap.start()
 while True:
