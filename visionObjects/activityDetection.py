b410a3e27af469bdce943f80b84566a7b0f0414e
Shouvik Patra <shouvikpatra@gmail.com>
2020-05-20 17:39:14 +0530

    Activity threshold added (Experimental)

--- visionObjects/activityDetection.py
+++ visionObjects/activityDetection.py
@@ -12,7 +12,9 @@ def activity_filter(current_frame, background_frame):
     frame_diff = cv2.absdiff(background_frame, current_frame)
     # gray = cv2.cvtColor(frame_diff, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(frame_diff, (5, 5), 0)
-    thresh_delta = cv2.threshold(blur, 50, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    thresh_delta = cv2.threshold(
+        blur, 50, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    activity = thresh_delta.copy()
 
     #kernel = np.ones((5,5),np.uint8)
     #erosion = cv2.erode(threst_delta,kernel,iterations = 1)
@@ -21,23 +23,28 @@ def activity_filter(current_frame, background_frame):
         dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     # Calculate centers of each contours
-    return (contours, calc_centers(contours, current_frame), dilated , frame_diff)
+    return (contours, calc_centers(contours, current_frame), dilated, frame_diff)
 
 
 def calc_centers(contours, current_frame):
     centers = []
     boxed_current_frame = current_frame.copy()
+    totalContourArea = 0
+    thresh_activity = 0.5
     for cnt in contours:
         (x, y, w, h) = cv2.boundingRect(cnt)
+        totalContourArea += w * h
         # filtering out which contours to ignore
-        if cv2.contourArea(cnt) < 500 or w > 100 or h > 150:
+        if cv2.contourArea(cnt) < 500:
             continue
-
         cv2.rectangle(boxed_current_frame, (x-1, y-1),
                       (x+w-1-1, y+h-1-1), (255, 0, 0), 3)
         M = cv2.moments(cnt)
         cx = int(M['m10'] / M['m00'])
         cy = int(M['m01'] / M['m00'])
         centers.append([cx, cy])
-
+    # TO ignore results due to camera Vibrations
+    if totalContourArea > (current_frame.shape[0]*current_frame.shape[1])*thresh_activity:
+        centers = []
+        boxed_current_frame = current_frame
     return centers, boxed_current_frame
