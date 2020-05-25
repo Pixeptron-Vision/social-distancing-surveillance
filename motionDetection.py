7cb880e194a266da7cb976b048a9ac65084bc9bc
mukuakhilesh <akhi.biada007@gmail.com>
2020-05-25 18:25:36 +0530

    Add MultiProcessing for multiple cams

--- motionDetection.py
+++ motionDetection.py
@@ -42,8 +42,8 @@ class VisionSurveillance:
 
 
     def initialize_first_frame(self):
-        # ret,frame = self.cap.read()
-        frame = cv2.imread('stream_error.jpg')
+        ret,frame = self.cap.read()
+        # frame = cv2.imread('stream_error.jpg')
         if frame is None:
             print("[INFO] Stream unavailable..!")
             user_exit = display_obj.display_error()
@@ -67,12 +67,14 @@ class VisionSurveillance:
         self.display_obj = FrameDisplay().start()
         self.display_obj.index=index_count
         self.cap = VideoCaptureAsync(src=self.src).start()
+        self.initialize_first_frame()
+
         while self.started:
 
             ret, current_frame = self.cap.read()
             if not ret or current_frame is None:
                 print("[INFO] Cam IP Stream unavailable...")
-                user_exit = display_obj.display_error()
+                user_exit = self.display_obj.display_error()
 
             else:
                 # print("fid - ", fid)
@@ -107,6 +109,7 @@ class VisionSurveillance:
                 # Choosing next frame
                 self.prev_frame = current_frame
                 self.fid += 1
+
             if user_exit == True:
                 self.started=False
                 self.cap.stop()
@@ -123,13 +126,13 @@ if __name__ == '__main__':
     #       at the time of frame display...as windows are named with index
     #       to avoid mixing and overriding of frames during display.
     det1 = VisionSurveillance(src='../PNNLParkingLot2.avi')
-    det2 = VisionSurveillance(src='../PNNL_Parking_LOT(1).avi')
-    det3 = VisionSurveillance(src='../PNNLParkingLot2.avi')
-    det4 = VisionSurveillance(src='../vid1.mp4')
-    det1.initialize_first_frame()
-    det2.initialize_first_frame()
-    det3.initialize_first_frame()
-    det4.initialize_first_frame()
+    det2 = VisionSurveillance(src='../vid1.mp4')
+    det3 = VisionSurveillance(src='../vid2.mp4')
+    det4 = VisionSurveillance(src='../PNNL_Parking_LOT(1).avi')
+    # det1.initialize_first_frame()
+    # det2.initialize_first_frame()
+    # det3.initialize_first_frame()
+    # det4.initialize_first_frame()
 
     # li = [det1,det2,det3]
     li = [det1,det2,det3,det4]
